d733e256b3733b545a17414137738f0125daa43b
Scott Williams <scott@mightydevco.com>
2022-05-01 09:33:33 -0500

    addin gGripes

---
+++ src/pages/gripe.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement, JSXElementConstructor, ReactFragment, ReactPortal, FunctionComponent} from "react"
+import {LinkProps} from "next/dist/client/link";
+
+
+export const Gripe =  (
+  {children}
+                               ) => (
+    <>{children}</>
+);
+
+export default Gripe
--- src/pages/presentation.tsx
+++ src/pages/presentation.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import present from '@/styles/Presentation.module.css';
 import Link from "next/link";
+import Gripe from "@/pages/gripe";
 
 export default function Home() {
   return (
@@ -25,12 +26,22 @@ export default function Home() {
       </Head>
 
       <header id={present.header}>
-        <Link href="/"><h1>MECHTRON</h1></Link>
+        <span><Link href="/"><h1>MECHTRON</h1></Link></span>
       </header>
 
       <main id={present.main}>
         <h1>DevOps Gripes</h1>
-
+        <section>
+          <h2>1) Software Installation</h2>
+          <ul>
+            <li>
+              <h3>Dependency Confusion</h3>
+              <Gripe>Hello</Gripe>
+              <p>Applications are often dependent upon a properly configured environment and even other applications to be preinstalled (such as a database).
+                When they fail to install there is no single actionable error message so DevOps engineers are required to review various status messages, logs and test theories to determine why the software isn't working as expected.</p>
+            </li>
+          </ul>
+        </section>
       </main>
 
     </>
